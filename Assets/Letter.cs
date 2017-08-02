b93c7552d91fec2662db9877a26e6e0a75b50a58
Andrew Graham <andrew.graham@kwalee.com>
2017-08-02 18:16:41 +0100

    first commit

---
+++ Assets/Letter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Letter : MonoBehaviour {
+
+	[SerializeField]
+	Text m_text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetLetter(char letter)
+	{
+		m_text.text = letter.ToString();
+	}
+}
---
+++ Assets/LetterRing.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterRing : MonoBehaviour 
+{
+	[SerializeField]
+	GameObject m_letterPrefab;
+
+	[SerializeField]
+	float m_radiusOfRing;
+
+	[SerializeField]
+	char[] m_lettersToBeUsed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		SetUpRing();
+	}
+
+	public void SetUpRing()
+	{
+		DeleteExistingLetters();
+
+		int numLetters = m_lettersToBeUsed.Length;
+		for(int i=0; i<numLetters; i++)
+		{
+			float angle = (Mathf.PI*2*i)/numLetters;
+			Vector2 pos = new Vector2(m_radiusOfRing * Mathf.Sin(angle), m_radiusOfRing * Mathf.Cos(angle));
+			GameObject letter = Instantiate(m_letterPrefab,transform);
+			letter.transform.localPosition = pos;
+			letter.GetComponent<Letter>().SetLetter(m_lettersToBeUsed[i]);
+		}
+	}
+
+	void DeleteExistingLetters()
+	{
+		Letter[] letters = transform.GetComponentsInChildren<Letter>();
+		foreach(Letter b in letters)
+		{
+			b.transform.SetParent(null);
+			Destroy(b.gameObject);
+		}
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
